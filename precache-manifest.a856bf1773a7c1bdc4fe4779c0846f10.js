bd53706e514b7f0f0fd24e98c26b1a3456a9671f
baraiya lakshit <lakshitbaraiya@gmail.com>
2023-08-21 09:19:04 +0530

    Updates

---
+++ precache-manifest.a856bf1773a7c1bdc4fe4779c0846f10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2490f7b18fa594770eec",
+    "url": "/Task-Tracker/static/css/main.0b0f6048.chunk.css"
+  },
+  {
+    "revision": "2490f7b18fa594770eec",
+    "url": "/Task-Tracker/static/js/main.934ad6df.chunk.js"
+  },
+  {
+    "revision": "3b0a145527451fdf3824",
+    "url": "/Task-Tracker/static/js/runtime~main.1f80ad29.js"
+  },
+  {
+    "revision": "b53cfbb84ef4209bca5e",
+    "url": "/Task-Tracker/static/js/2.3dba1a0f.chunk.js"
+  },
+  {
+    "revision": "c563299317d52c1696cd",
+    "url": "/Task-Tracker/static/js/3.937f6cc8.chunk.js"
+  },
+  {
+    "revision": "301029d57fcddaddcbf3fcc83f942990",
+    "url": "/Task-Tracker/index.html"
+  }
+];
